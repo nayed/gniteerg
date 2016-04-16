02b728a90c1c8620e7632e077adb83d6d07fa1ff
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-04-16 23:05:10 +0200

    .

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import Hapi from 'hapi'
+import uuid from 'uuid'
 
 let server = new Hapi.Server()
+
+let cards = {}
 
 server.connection({ port: 3000 })
 
@@ -45,17 +48,42 @@ server.register(require('inert'), (err) => {
         handler: cardsHandler
     })
 
+    server.route({
+        path: '/cards/{id}',
+        method: 'DELETE',
+        handler: deleteCardHandler
+    })
+
     function newCardHandler(request, reply) {
         if(request.method === 'get') {
             reply.file('templates/new.html')
         }
         else {
+            let card = {
+                name: request.payload.name,
+                recipient_email: request.payload.recipient_email,
+                sender_name: request.payload.sender_name,
+                sender_email: request.payload.sender_email,
+                card_image: request.payload.card_image
+            }
+            saveCard(card)
+            console.log(cards)
             reply.redirect('/cards')
         }
     }
 
     function cardsHandler(request, reply) {
         reply.file('templates/cards.html')
+    }
+
+    function saveCard(card) {
+        let id = uuid.v1()
+        card.id = id
+        cards[id] = card
+    }
+
+    function deleteCardHandler(request, reply) {
+        delete cards[request.params.id]
     }
 
     server.start((err) => {
